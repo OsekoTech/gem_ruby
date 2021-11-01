f22a4fa1e5825045d36e6730615bf0cc718ea037
Sergio Oseko <osk.sergio@gmail.com>
2021-11-01 05:24:11 -0300

    build: bundle gem gem_ruby - first commit

---
+++ lib/gem_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gem_ruby/version"
+
+module GemRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gem_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GemRuby
+  VERSION = "0.1.0"
+end
