f066e304cb0b801d2a990869fe7586fc0b13b515
Sergio Oseko <osk.sergio@gmail.com>
2021-11-02 06:24:04 -0300

    chore: removed unnecessary comments

--- lib/fase1/bubble_sort.rb
+++ lib/fase1/bubble_sort.rb
@@ -20,31 +20,3 @@ module Bubble
     end
   end
 end
-
-=begin
-arr1 = [64, 34, 25, 12, 22, 11, 90]
-
-bubbleSort(arr1)
-
-puts 'Sorted array is: '
-
-(0..arr1.count - 1).each do |i|
-  puts(arr1[i])
-end
-=end
-
-=begin
-    =begin
-    def bubbleSort(arr)
-      n = arr.count
-
-      range = 0..(n - 1)
-
-      range.each do |i|
-        (0..((n - 1) - i - 1)).each do |j|
-          arr[j], arr[j + 1] = arr[j + 1], arr[j] if arr[j] > arr[j + 1]
-        end
-      end
-    end
-    =end
-=end
